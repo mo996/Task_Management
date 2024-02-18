a63d4e967efd19d81249805b37c36c12ff66f0ff
mo996 <74605524+mo996@users.noreply.github.com>
2024-02-18 16:05:47 +0100

    Delete src directory

--- src/main/java/com/daaeboul/taskmanagementsystem/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.daaeboul.taskmanagementsystem.repository;
-
-import com.daaeboul.taskmanagementsystem.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- src/main/java/com/daaeboul/taskmanagementsystem/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.daaeboul.taskmanagementsystem.repository;
-
-import com.daaeboul.taskmanagementsystem.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
